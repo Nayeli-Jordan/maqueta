01161b78e686615b4674b759c29b7a0a750a4dba
Nayeli Jordan <[nayeli.jordan16@gmail.com]>
2018-07-26 13:09:47 -0500

    Bases maqueta

---
+++ themes/custom-theme/footer.php
@@ -0,0 +1,7 @@
+			<footer>
+				<!-- <small> © <span id="year"></span> | <a href="https://gatchweb.com/" target="_blank">Hecho por GatchWeb</a></small> -->
+			</footer>
+		</div> <!-- end main-body -->
+		<?php wp_footer(); ?>
+	</body>
+</html>
