8e79577ca70bf8e0bc1638dcedf71602e029daa7
Nayeli Jordan <[nayeli.jordan16@gmail.com]>
2018-08-02 16:50:20 -0500

    template footer

--- themes/qo-theme/single-qo_cotizaciones.php
+++ themes/qo-theme/single-qo_cotizaciones.php
@@ -155,18 +155,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="footer-cotizacion">
-			<?php if( $iva_inc == "Sí" ) : ?>
-				<p class="inline-block">Precio cotizado con IVA</p>
-			<?php else: ?>
-				<p class="inline-block">Precio cotizado <strong>sin</strong> IVA</p>
-			<?php endif; ?>			
-			<p class="inline-block">Anticipo del 50%</p>
-			<p class="inline-block">Vigencia 15 días</p>
-			<br>
-			<div class="bg-image bg-contain bg-qo-logo" style="background-image: url(<?php echo THEMEPATH; ?>images/identidad/logo.png)"></div>
-			<p>Atentamente:<br>Control y administración<br>¿Qué Onda?</p>
-		</div>
+		<?php include (TEMPLATEPATH . '/templates-cotizacion/qo-footer.php'); ?>
 	</div>	
 <?php 
 	endwhile; // end of the loop.
