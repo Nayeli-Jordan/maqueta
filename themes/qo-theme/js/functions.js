4aa56b0b908c31b5f99c0a1c5de9473d87c69872
Nayeli Jordan <[nayeli@queonda.com.mx]>
2018-10-12 15:47:27 -0500

    eliminada función de isotope filtros simples

--- themes/qo-theme/js/functions.js
+++ themes/qo-theme/js/functions.js
@@ -116,31 +116,6 @@ var $=jQuery.noConflict();
 	});
 })(jQuery);
 
-/*Filtros CotizaciónQO (simple)*/
-
-function isotopeQO(){
-	// init Isotope
-	var $grid = $('.grid').isotope({
-	  itemSelector: '.element-item',
-	  layoutMode: 'fitRows'
-	});
-	// bind filter button click
-	$('#filters').on( 'click', 'button', function() {
-	  var filterValue = $( this ).attr('data-filter');
-	  // use filterFn if matches value
-	  filterValue = /*filterFns[ filterValue ] ||*/ filterValue;
-	  $grid.isotope({ filter: filterValue });
-	});
-
-	// change is-checked class on buttons
-	$('.button-group').each( function( i, buttonGroup ) {
-	  var $buttonGroup = $( buttonGroup );
-	  $buttonGroup.on( 'click', 'button', function() {
-	    $buttonGroup.find('.is-checked').removeClass('is-checked');
-	    $( this ).addClass('is-checked');
-	  });
-	});
-}
 
 /*Masonry galería*/
 function imageMasonry(){
